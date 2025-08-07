910225c028083b8c05c2d27549e6390f5ceaddfe
joaobarto <joaobfn12@gmail.com>
2025-08-07 13:56:39 -0300

    Finalizando

--- obj/Debug/net8.0/MinhaAppMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinhaAppMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaAppMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2d4ad67fb5e02d3dffd190addfda1da76681f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c475a4286dbcceab976850e38d0d98d75d95a082")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaAppMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaAppMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
